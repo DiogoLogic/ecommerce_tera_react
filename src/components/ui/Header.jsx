746cb2988b6582e7a2adb712cac269502ed529c4
DiogoLogic <diogologic@gmail.com>
2023-11-07 00:12:49 -0300

    configuração do projeto concluída

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,66 @@
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCart,
+} from "lucide-react";
+import { Button } from "./button";
+import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+
+const Header = () => {
+  return (
+    <Card className="shadow-black bg-white-700 flex justify-between p-[1.875rem] items-center">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-2">
+            <Button className="w-full justify-start gap-3" variant="outline">
+              <LogInIcon size={16} />
+              Fazer Login
+            </Button>
+
+            <Button className="w-full justify-start gap-2" variant="outline">
+              <HomeIcon size={16} />
+              Início
+            </Button>
+
+            <Button className="w-full justify-start gap-2" variant="outline">
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+
+            <Button className="w-full justify-start gap-2" variant="outline">
+              <ListOrderedIcon  size={16} />
+              Catalogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <h1
+        className=" text-green-300 text-3xl font-extrabold
+       "
+      >
+        <span className="text-primary text-green-300">Game</span>Versus 
+        
+      </h1>
+      <Button size="icon" variant="outline">
+        <ShoppingCart />
+      </Button>
+    </Card>
+  );
+};
+
+export default Header;
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/Ecommerce_Tera_React/ecommerce_tera_react/src',// Substitua pelo caminho correto para o diretório de origem
+    },
+  },
+});
